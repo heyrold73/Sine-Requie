6f6214c835f04aa95bdb98363a7410227ba9272c
heyrold73 <63519965+heyrold73@users.noreply.github.com>
2023-12-15 09:39:19 +0100

    Delete module directory

--- module/sheets/items/_equippable-item-template-sheet.js
+++
@@ -1,8 +0,0 @@
-import { EquippableItemSheet } from './equippable-item-sheet.js';
-
-/**
- * Extend the basic EquippableItemSheet with some very simple modifications
- * @extends {EquippableItemSheet}
- * @ignore
- */
-export class EquippableItemTemplateSheet extends EquippableItemSheet {}
--- module/sheets/template-sheet.js
+++
@@ -1,8 +0,0 @@
-import { CustomActorSheet } from './actor-sheet.js';
-
-/**
- * Extend the basic ActorSheet with some very simple modifications
- * @extends {CustomActorSheet}
- * @ignore
- */
-export class TemplateSheet extends CustomActorSheet {}
